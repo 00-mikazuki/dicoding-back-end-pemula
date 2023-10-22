4bb89d11b8ac7e49a24e661201977510f3a3deeb
00-mikazuki <diojuan432@gmail.com>
2023-10-22 19:07:35 +0700

    33%

---
+++ javascript-projects/nodejs-basic/filesystem/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const { resolve } = require('path');
+
+const fileReadCallback = (error, data) => {
+  if(error) {
+    console.log('Gagal membaca berkas');
+    return;
+  }
+  console.log(data);
+};
+
+fs.readFile(resolve(__dirname, 'notes.txt'), 'UTF-8', fileReadCallback);
+
+// sync
+const data = fs.readFileSync(resolve(__dirname, 'notes.txt'), 'UTF-8');
+console.log(data);
